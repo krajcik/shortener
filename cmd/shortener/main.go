6ce04ec77de1e3054130a9631d04f7ee46640440
Nikita Krajcik <krajcik.nikita@gmail.com>
2024-04-19 12:48:41 +0300

    Add resty and chi

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"krajcik/shortener/cmd/shortener/handler"
 	"krajcik/shortener/internal/app/shortener"
 	"net/http"
+	"time"
 )
 
 var service = shortener.NewService(shortener.NewRepository())
@@ -15,14 +18,24 @@ func main() {
 }
 
 func run() error {
-	return http.ListenAndServe(":8080", http.HandlerFunc(webhook))
+
+	return http.ListenAndServe(":8080", router())
 }
 
-func webhook(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		handler.PostShrt(service)(w, r)
-		return
-	}
+func router() chi.Router {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.URLFormat)
+	r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(middleware.NoCache)
+	r.Use(middleware.Heartbeat("/ping"))
+	r.Use(middleware.SetHeader("Content-Type", "plain/text; charset=utf-8"))
+	r.Use(middleware.Recoverer)
 
-	handler.GetShrt(service)(w, r)
+	r.Get("/{shrt}", handler.GetShrt(service))
+	r.Post("/", handler.PostShrt(service))
+
+	return r
 }
