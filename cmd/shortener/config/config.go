c090806b869e30e2d64dc4aac913b854357965a1
Nikita Krajcik <krajcik.nikita@gmail.com>
2024-04-20 07:24:42 +0300

    Env

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -1,11 +1,20 @@
 package config
 
 import "flag"
+import "github.com/caarlos0/env/v11"
 
 func Create() *Params {
 	p := new(Params)
-	flag.StringVar(&p.A, "a", ":8080", "The address to listen on for HTTP requests.")
-	flag.StringVar(&p.B, "b", "", "Host for shorten url")
+	err := env.Parse(p)
+	if err != nil {
+		panic(err)
+	}
+	if p.A == "" {
+		flag.StringVar(&p.A, "a", ":8080", "The address to listen on for HTTP requests.")
+	}
+	if p.B == "" {
+		flag.StringVar(&p.B, "b", "", "Host for shorten url")
+	}
 
 	flag.Parse()
 
